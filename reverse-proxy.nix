092f0d5d6dad10e9b72e147df8515d7f35ed0f90
SamLukeYes <samlukeyes123@gmail.com>
2022-05-03 13:50:19 +0800

    update comments in reverse-proxy.nix

--- reverse-proxy.nix
+++ reverse-proxy.nix
@@ -1,8 +1,6 @@
 # Modify this file to use your own reverse proxy,
 # or use an empty string to disable it.
 
-# Currently, this is only used to download the pacman source tarball.
-
 "https://rp.samlukeyes123.workers.dev/"
 
 # Backend: https://gitlab.com/NickCao/experiments/-/blob/master/workers/r.js
